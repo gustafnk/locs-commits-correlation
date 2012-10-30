6cebadd06b94ff0d4f36913d6f65d2c309a5859d
Gustaf Nilsson Kotte <gustaf.nk@gmail.com>
2012-10-30 08:02:03 +0100

    Initial commit

---
+++ locsAndCommits.js
@@ -0,0 +1,39 @@
+
+var metrics = [
+	{loc: 1, commits: 2},
+	{loc: 2, commits: 4},
+	{loc: 3, commits: 6},
+	{loc: 4, commits: 8},
+	{loc: 5, commits: 10},
+	{loc: 6, commits: 12},
+	{loc: 7, commits: 14},
+	{loc: 8, commits: 16},
+	{loc: 9, commits: 18},
+	{loc: 10, commits: 20}
+];
+
+// TODO filter out loc === 0 or commits === 0
+
+var locSum = metrics.map(function(item){
+	return item.loc;
+}).reduce(function(m,n){
+	return m+n;
+});
+
+metrics.forEach(function(item){ 
+	item.locPercent = item.loc/locSum;
+});
+
+var sum = 0;
+metrics.forEach(function(item){
+	sum += item.loc; 
+	item.locAccSum = sum;
+});
+
+// Print the result matrix
+metrics.forEach(function(item){
+	console.log("LOC: ", item.loc, "; Commits: ", item.commits, "; LOCPercent: ", item.locPercent, "; LOCAccSum: ", item.locAccSum);
+});
+
+console.log();
+console.log("LOC sum:", locSum);
