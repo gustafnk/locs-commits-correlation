f734586d5aa9569d7dc3a4f46bb2ec658379fb78
Gustaf Nilsson Kotte <gustaf.nk@gmail.com>
2012-11-26 20:09:56 +0100

    Added main shell script

---
+++ locs_commits_correlation.sh
@@ -0,0 +1,12 @@
+locs_and_commits.sh $1 $2 > tmp
+DIRECTORY="out"
+
+# name=basename "$1" # TODO Learn more about basename
+# name+=".html"
+
+if [ ! -d "$DIRECTORY" ]; then
+    mkdir "$DIRECTORY"
+fi
+
+node locsAndCommits > "$DIRECTORY/result.html"
+rm tmp
