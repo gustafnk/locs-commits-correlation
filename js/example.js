f5065557bb57eb597308e0ec67fae1c70a5dc30c
Gustaf Nilsson Kotte <gustaf.nk@gmail.com>
2012-11-26 20:11:05 +0100

    Added jQuery Visualize files and result template

---
+++ js/example.js
@@ -0,0 +1,15 @@
+// Run the script on DOM ready:
+$(function(){
+	$('table.locs').visualize({type: 'bar', width: '960px', height: '400px', parseDirection: 'x', colFilter: ':eq(0)'});
+    $('table.commits').visualize({type: 'bar', width: '960px', height: '400px', parseDirection: 'x', colFilter: ':eq(0)'});
+    
+    $('table.full').visualize({type: 'bar', width: '960px', height: '400px', parseDirection: 'x', colFilter: ':eq(5)'});
+    $('table.full').visualize({type: 'bar', width: '960px', height: '400px', parseDirection: 'x', colFilter: ':eq(4)'});
+    $('table.full').visualize({type: 'bar', width: '960px', height: '400px', parseDirection: 'x', colFilter: ':eq(3)'});
+    $('table.full').visualize({type: 'bar', width: '960px', height: '400px', parseDirection: 'x', colFilter: ':eq(2)'});
+    
+
+    $('table.locs').hide();
+    $('table.commits').hide();
+    $('table.full').hide();
+});
