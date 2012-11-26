1b7c922099aa61df850c868d14264c42958a9832
Gustaf Nilsson Kotte <gustaf.nk@gmail.com>
2012-11-26 07:40:47 +0100

    Broke out parameters, now possible to point out a path and file pattern

--- locs_and_commits.sh
+++ locs_and_commits.sh
@@ -1,4 +1,5 @@
-git log --pretty=format: --name-only -- *.cs| sort | uniq -c | sort -rg | while read n file; do
-	count=`wc -l "$file" || echo "0"`
-	echo "$file $n $count"
+(cd $1; git log --pretty=format: --name-only -- $2) | sort | uniq -c | sort -rg | while read n file; do
+    (cd $1;
+	loc=`wc -l "$file" || echo "0"`;
+	echo "$file $n $loc")
 done
