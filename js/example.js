7e2b519bf4075888bf130fb2880efc82fb71383d
Gustaf Nilsson Kotte <gustaf.nk@gmail.com>
2012-11-27 17:51:38 +0100

    Refactored

--- js/example.js
+++ js/example.js
@@ -1,14 +1,23 @@
 // Run the script on DOM ready:
 $(function(){
-	$('table.locs').visualize({type: 'dot', width: '960px', height: '400px', parseDirection: 'x', colFilter: ':eq(0)'});
-    $('table.commits').visualize({type: 'dot', width: '960px', height: '400px', parseDirection: 'x', colFilter: ':eq(0)'});
+
+    var defaults = {
+            type: 'dot', 
+            width: '960px', 
+            height: '400px', 
+            parseDirection: 'x', 
+            colFilter: ':eq(0)'
+        };
+
+	$('table.locs').visualize($.extend($.extend({}, defaults), { colFilter: ':eq(0)'}));
+    $('table.commits').visualize($.extend($.extend({}, defaults), { colFilter: ':eq(0)'}));
     
-    $('table.full').visualize({type: 'dot', width: '960px', height: '400px', parseDirection: 'x', colFilter: ':eq(5)'});
+    $('table.full').visualize($.extend($.extend({}, defaults), { colFilter: ':eq(5)'}));
     
-    $('table.full').visualize({type: 'dot', width: '960px', height: '400px', parseDirection: 'x', colFilter: ':eq(3)'});
-    $('table.full').visualize({type: 'dot', width: '960px', height: '400px', parseDirection: 'x', colFilter: ':eq(4)'});
-    $('table.full').visualize({type: 'dot', width: '960px', height: '400px', parseDirection: 'x', colFilter: ':eq(2)'});
-    $('table.full').visualize({type: 'dot', width: '960px', height: '400px', parseDirection: 'x', colFilter: ':eq(6)'});
+    $('table.full').visualize($.extend($.extend({}, defaults), { colFilter: ':eq(3)'}));
+    $('table.full').visualize($.extend($.extend({}, defaults), { colFilter: ':eq(4)'}));
+    $('table.full').visualize($.extend($.extend({}, defaults), { colFilter: ':eq(2)'}));
+    $('table.full').visualize($.extend($.extend({}, defaults), { colFilter: ':eq(6)'}));
 
     $('table.locs').hide();
     $('table.commits').hide();
