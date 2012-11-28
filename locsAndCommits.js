564a8e0496feb39f2f39c238d8f38871cf961fe4
Gustaf Nilsson Kotte <gustaf.nk@gmail.com>
2012-11-28 13:23:26 +0100

    Showing linear regression values: k, m and r^2

--- locsAndCommits.js
+++ locsAndCommits.js
@@ -14,6 +14,48 @@ if (!data)
         message: "Something went wrong... No data in file or file not found."
     };
 
+// TODO Extract function to separate file
+function leastSquares(xs, ys) {
+    
+    $_.sum = function(numbers){
+        return $_.reduce(numbers, function(memo, number){
+            return memo + number;
+        }, 0);
+    };
+
+    var xsSum = $_.sum(xs);
+    var ysSum = $_.sum(ys);
+
+    var xsSquaredSum = $_.sum(xs.map(function(x){
+        return x*x;
+    }));
+
+    var ysSquaredSum = $_.sum(ys.map(function(y){
+        return y*y;
+    }));
+
+    var valuePairs = $_.zip(xs, ys);
+    var xsTimesYsSum = $_.sum(valuePairs.map(function(pair){
+        return pair[0]*pair[1];
+    }));
+
+    var count = valuePairs.length;
+    
+    // Calculate m and b for the equation y = x * m + b 
+    var slope = (count*xsTimesYsSum - xsSum*ysSum) / (count*xsSquaredSum - xsSum*xsSum);
+    var intercept = (ysSum/count) - (slope*xsSum)/count;
+    var rSquared = Math.pow((count*xsTimesYsSum - xsSum*ysSum)/Math.sqrt((count*xsSquaredSum-xsSum*xsSum)*(count*ysSquaredSum-ysSum*ysSum)),2);
+
+    return {
+        slope: slope,
+        intercept: intercept,
+        rSquared: rSquared,
+        toString: function(){
+            return "y = k*x + m, where\n\n" + "k = " + slope + "\nm = " + intercept;
+        }
+    };
+}
+
 var metricsNotSorted =
     $_.chain(lines)
         .tail()
@@ -31,13 +73,20 @@ var metricsNotSorted =
 
 var metrics = $_.sortBy(metricsNotSorted, "loc");
 
+var locs = $_.map(metrics, function(metric){return metric.loc;});
+var commits = $_.map(metrics, function(metric){return metric.commits;});
+
+var linearRegression = leastSquares(locs, commits);
+
 var template = fs.readFileSync("template.jade", 'utf8');
 var template_loc = fs.readFileSync("template_loc.jade", 'utf8');
 var template_commits = fs.readFileSync("template_commits.jade", 'utf8');
+var template_rsquared = fs.readFileSync("template_rsquared.jade", 'utf8');
 
 var fn = jade.compile(template);
 var fn_loc = jade.compile(template_loc);
 var fn_commits = jade.compile(template_commits);
+var fn_rsquared = jade.compile(template_rsquared)
 
 var sumForProperty = function(property){
     return metrics.map(function(item){
@@ -124,6 +173,7 @@ writeAccumulatedSumForProperty(sortByReverseLocs, "commitsPercent");
 
 
 var str = 
+    fn_rsquared(linearRegression) + "\n\n" +
     fn_loc({metrics: sortByLocs}) + "\n\n" +
     fn_commits({metrics: sortByCommits}) + "\n\n" +
     fn({metrics: sortByReverseLocs});
@@ -133,8 +183,6 @@ var result = htmlTemplate.replace("<!-- placeholder for tables -->", str);
 
 console.log(result);
 
-/*
-sortByLocs.forEach(function(metric){
-    console.log(metric.locPercent + "%, " + metric.locPercentAccSum);  
-})
-*/
+//console.log("slope: " + result.slope);
+//console.log("intercept: " + result.intercept);
+//console.log("rSquared: " + result.rSquared);
