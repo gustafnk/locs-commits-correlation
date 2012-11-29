4b787ef39d44b66feefbbfbc0f2321f8ed37655b
Gustaf Nilsson Kotte <gustaf.nk@gmail.com>
2012-11-29 08:43:18 +0100

    Refactored

--- locs_and_commits.sh
+++ locs_and_commits.sh
@@ -1,7 +1,9 @@
 current_path=$PWD
-cd "$1"; git log --pretty=format: --name-only -- "$2" | sort | uniq -c | sort -rg | while read n file; do
-    cd "$1";
-	loc=`wc -l "$file" || echo "0"`;
-	echo "$file $n $loc"
-done
+cd "$1"; 
+git log --pretty=format: --name-only -- "$2" | 
+    sort | uniq -c | sort -rg | while read n file; 
+    do
+    	loc=`wc -l "$file" || echo "0"`;
+    	echo "$file $n $loc"
+    done
 cd "$current_path"
