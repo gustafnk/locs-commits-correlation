142dc4443f2a2ef61233162b89b0a974323c49e1
Gustaf Nilsson Kotte <gustaf.nk@gmail.com>
2013-04-02 15:04:53 +0200

    Showing data tables. Show name in summary.

--- js/app.js
+++ js/app.js
@@ -2,9 +2,9 @@
 $(function(){
 
     var defaults = {
-            type: 'dot', 
-            width: '960px', 
-            height: '400px', 
+            type: 'dot',
+            width: '960px',
+            height: '400px',
             parseDirection: 'x',
             colors: ["#e9e744"]
         };
@@ -13,16 +13,16 @@ $(function(){
     $('table.commits').visualize($.extend($.extend({}, defaults), { colFilter: ':eq(1)'}));
     $('table.factor').visualize($.extend($.extend({}, defaults), { colFilter: ':eq(1)'}));
     $('table.density').visualize($.extend($.extend({}, defaults), { colFilter: ':eq(1)'}));
-    
+
     $('table.full').visualize($.extend($.extend({}, defaults), { colFilter: ':eq(5)'}));
-    
+
     $('table.full').visualize($.extend($.extend({}, defaults), { colFilter: ':eq(3)'}));
     $('table.full').visualize($.extend($.extend({}, defaults), { colFilter: ':eq(4)'}));
     $('table.full').visualize($.extend($.extend({}, defaults), { colFilter: ':eq(2)'}));
-    
-    $('table.locs').hide();
-    $('table.commits').hide();
-    $('table.factor').hide();
-    $('table.density').hide();
-    $('table.full').hide();
+
+    // $('table.locs').hide();
+    // $('table.commits').hide();
+    // $('table.factor').hide();
+    // $('table.density').hide();
+    // $('table.full').hide();
 });
