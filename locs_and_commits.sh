dbfb189e15c29a1ab8c988e7ef2c3dff569fdf13
Gustaf Nilsson Kotte <gustaf.nk@gmail.com>
2013-11-11 16:25:51 +0100

    Remove old files

--- locs_and_commits.sh
+++
@@ -1,9 +0,0 @@
-current_path=$PWD
-cd "$1"; 
-git log --pretty=format: --name-only -- "$2" | 
-    sort | uniq -c | sort -rg | while read n file; 
-    do
-    	loc=`wc -l "$file" || echo "0"`;
-    	echo "$file $n $loc"
-    done
-cd "$current_path"
